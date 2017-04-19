2dc371cbead657a2b4afeeaec4b74c817bbfe96d
itslavrov <itslavrov@me.com>
2017-04-19 22:17:09 +0400

    add virtual function and used for class child and base

--- main.cpp
+++ main.cpp
@@ -7,16 +7,30 @@ class Base
 public:
     Base() { cout << "Construct Base object\n"; }
     ~Base() { cout << "Destruct Base object\n"; }
+    void func(){cout<<"Function func() of class base\n";}
 };
-class Child:public Base
+
+class Child: public Base
 {
 public:
-child(){cout<<"construct Child object\n";}
-~child(){cout<<"destruct Child object\n;}
+    Child() { cout << "Construct Child object\n"; }
+    ~Child() { cout << "Destruct Child object\n"; }
 };
+
 int main(int argc, char *argv[])
 {
-    cout<<"*Create Object child"<<end;
+    cout<<"*Create Object base"<<endl;
+    Base base;
+    cout << "* Create Object child" << endl;
     Child child;
+    
+    cout<<"*call method func() for object base"<<endl;
+    base.func();
+    
+    cout<<"*call method func() for object child"<<endl;
+    
+    child.func();
+        
+
     return 0;
 }
