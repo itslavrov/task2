917db59eca7276887a90f777fc89660c0cff4cb2
itslavrov <itslavrov@me.com>
2017-04-19 22:02:20 +0400

    added class and constructor

--- main.cpp
+++ main.cpp
@@ -8,11 +8,15 @@ public:
     Base() { cout << "Construct Base object\n"; }
     ~Base() { cout << "Destruct Base object\n"; }
 };
-
+class Child:public Base
+{
+public:
+child(){cout<<"construct Child object\n";}
+~child(){cout<<"destruct Child object\n;}
+};
 int main(int argc, char *argv[])
 {
-    cout << "* Create Object base" << endl;
-    Base base;
-
+    cout<<"*Create Object child"<<end;
+    Child child;
     return 0;
 }
